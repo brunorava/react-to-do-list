61e8d66c2fe126fad8abe7102994f1026ba65692
Bruno Ravagnani <bruno.rava@gmail.com>
2022-10-14 11:42:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+cls# To do List
+
+Projeto em React + Typescript fazendo uma lista de tarefas.
+
+Projeto desenvolvido durante o curso de React JS da [B7Web](http://b7web.com.br).
+
+## Instalação
+- `npm install`
+
+## Para rodar
+- `npm start`
---
+++ src/components/AddArea/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 1px solid #555;
+    border-radius: 15px;
+    padding: 10px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    
+
+    .image {
+        margin-right: 5px;
+    }
+
+    input {
+        border: 0px;
+        outline: 0;
+        background-color: transparent;
+        color: white;
+        font-size: 18px;
+        font-family: Arial, Helvetica, sans-serif;
+        flex: 1;
+    }
+`;
